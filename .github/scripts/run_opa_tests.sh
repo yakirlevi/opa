db044b9e37013774ac70702cd8a3ba8a71d53e4e
Or-Kedmi <52378734+Or-Kedmi@users.noreply.github.com>
2022-11-15 16:28:23 +0200

    Remove old aws policies folders, tests run in run_opa_tests.sh, and update readme file links; (#21)
    Co-authored-by: Or-Kedmi <Or.k@qualisystems.com>

--- .github/scripts/run_opa_tests.sh
+++ .github/scripts/run_opa_tests.sh
@@ -7,9 +7,3 @@ opa test ./terraform/azure/allowed_locations -v
 opa test ./terraform/azure/allowed_resource_types -v
 opa test ./terraform/azure/prohibited_vm_sizes -v
 opa test ./terraform/azure/only_private_blob_storage -v
-
-# TODO: Remove the those commands after Torque sync and old folders deletion 
-opa test ./terraform/allowed_regions -v
-opa test ./terraform/allowed_resource_types -v
-opa test ./terraform/aws_prohibited_instance_types -v
-opa test ./terraform/only_private_S3_buckets -v
